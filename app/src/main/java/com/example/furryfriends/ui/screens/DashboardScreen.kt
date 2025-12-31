358b548637ad3266276ef37c48aa4b0f6d051e1d
Brogrammer7 <ct10025@tutanota.com>
2025-12-30 19:46:54 -0800

    Create TopAppBar composable and update strings file

--- app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
@@ -1,14 +1,38 @@
 package com.example.furryfriends.ui.screens
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.furryfriends.R
 
 @Composable
 fun DashboardScreen(
+    titleText: String,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        "Dashboard Screen"
+    FurryFriendsAppBar(
+        titleText = stringResource(R.string.dashboard_screen_title)
     )
+}
+
+@Composable
+fun FurryFriendsAppBar(
+    titleText: String,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(titleText)
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+    )
+
+
 }
