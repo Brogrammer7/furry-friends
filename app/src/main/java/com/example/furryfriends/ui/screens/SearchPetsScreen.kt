6126bd264058088be7485d4c5f7aff684491f511
Brogrammer7 <ct10025@tutanota.com>
2026-01-08 04:27:22 -0800

    Add organization name to pet listings, style changes

--- app/src/main/java/com/example/furryfriends/ui/screens/SearchPetsScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/SearchPetsScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,9 +45,9 @@ fun SearchPetsScreen(
     viewModel: SearchPetsViewModel = viewModel()
 ) {
     val uiState by viewModel.searchUiState.collectAsState()
+
     val zipIntState by viewModel.zipState.collectAsState()
     val zipErrorState by viewModel.zipError.collectAsState()
-
     val zipText = if (zipIntState == -1) "" else zipIntState.toString()
 
     Column(modifier = modifier
@@ -68,18 +69,19 @@ fun SearchPetsScreen(
                 .padding(horizontal = 16.dp, vertical = 8.dp)
         )
 
-        Row {
+        Row (modifier = Modifier.padding(bottom = 8.dp)) {
             Button(
                 onClick = { viewModel.searchPetData(Species.CATS.type) },
-                modifier = Modifier.padding(vertical = 16.dp, horizontal = 16.dp)
+                modifier = Modifier.padding(horizontal = 16.dp)
             ) {
                 Text(
                     "Search pets"
                 )
             }
             Button(
-                onClick = { viewModel.clearSearchData() },
-                modifier = Modifier.padding(vertical = 16.dp, horizontal = 16.dp)
+                onClick = { viewModel.clearZip()
+                    viewModel.clearSearchData() },
+                modifier = Modifier.padding(horizontal = 16.dp)
             ) {
                 Text(
                     "Reset Results"
@@ -94,6 +96,7 @@ fun SearchPetsScreen(
             contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp)
         ) {
             val searchList = uiState.items?.data ?: emptyList()
+            val includedList = uiState.items?.included
             items(
                 items = searchList,
                 key = { it.attributes.name ?: searchList.indexOf(it) }
@@ -133,24 +136,32 @@ fun SearchPetsScreen(
                                 Text(
                                     text = proper,
                                     style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp),
+                                    textAlign = TextAlign.Center,
                                     modifier = Modifier.padding(top = 8.dp)
-                                )
-                            }
-                            animals.attributes.breedPrimary?.let { value ->
-                                Text(
-                                    text = value,
-                                    style = TextStyle(fontSize = 12.sp),
-                                    modifier = Modifier.padding(vertical = 4.dp)
                                 )
                             }
                             animals.attributes.ageString?.let { value ->
                                 Text(
                                     text = value,
                                     style = TextStyle(fontSize = 12.sp),
+                                    textAlign = TextAlign.Center,
                                     modifier = Modifier.padding(bottom = 8.dp)
                                 )
                             }
+                            // get first org relationship id for this resource (if any)
+                            val orgRelId = animals.relationships.orgs?.data?.firstOrNull()?.id
+                            // find included org by id and type "orgs"
+                            val orgIncluded = includedList?.find { it.id == orgRelId && it.type == "orgs" }
+
+                            orgIncluded?.attributes?.name?.let { orgName ->
+                                Text(
+                                    text = orgName,
+                                    style = TextStyle(fontSize = 12.sp),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
                         }
+
                         ShareLinkButton222(
                             linkUrl = animals.attributes.pictureThumbnailUrl,
                             petName = animals.attributes.name,
--- app/src/main/java/com/example/furryfriends/ui/viewmodels/SearchPetsViewModel.kt
+++ app/src/main/java/com/example/furryfriends/ui/viewmodels/SearchPetsViewModel.kt
@@ -53,6 +53,7 @@ class SearchPetsViewModel: ViewModel() {
     }
 
     init {
+        clearZip()
         clearSearchData()
     }
 
