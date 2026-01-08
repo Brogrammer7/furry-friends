82f8bd0811d5bdeafa3ee4f09c49f4ef5e5fff83
Brogrammer7 <ct10025@tutanota.com>
2026-01-07 20:32:07 -0800

    Added SearchPetsScreen + associated ViewModel

---
+++ app/src/main/java/com/example/furryfriends/ui/viewmodels/SearchPetsViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.furryfriends.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.furryfriends.model.DataNode
+import com.example.furryfriends.model.FilterRadius
+import com.example.furryfriends.model.SearchRequest
+import com.example.furryfriends.model.SearchResponse
+import com.example.furryfriends.network.PetsApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.io.IOException
+import android.util.Log
+import com.example.furryfriends.network.Species
+import kotlinx.coroutines.Dispatchers
+
+data class SearchUiState(
+    val items: SearchResponse? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
+
+class SearchPetsViewModel: ViewModel() {
+
+    private val _searchUiState = MutableStateFlow(SearchUiState())
+    val searchUiState: StateFlow<SearchUiState> = _searchUiState.asStateFlow()
+
+    init {
+        clearSearchData()
+    }
+
+    fun searchPetData(petType: String, searchZip: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _searchUiState.update {
+                it.copy(
+                    isLoading = true,
+                    error = null) }
+            try {
+                val requestBody = SearchRequest(
+                    data = DataNode(
+                        //TODO server doesn't seem to acknowledge filters despite what documentation says
+//                    filters = listOf(
+//                        Filter("statuses.name", "equals", "Available"),
+//                        Filter("species.singular", "equals", "Cat"),
+//                        Filter("species.singular", "equals", "Dog"),
+//                    ),
+//                    filterProcessing = "1 AND (2 OR 3)",
+                        filterRadius = FilterRadius(
+                            miles = 10,
+                            postalCode = searchZip
+                        )
+                    )
+                )
+
+                val searchApiResult: SearchResponse = PetsApi.retrofitService.searchPets(
+                    body = requestBody,
+                    species = petType
+                )
+
+                _searchUiState.update {
+                    it.copy(
+                        items = searchApiResult,
+                        isLoading = false,
+                        error = null
+                    )
+                }
+
+            } catch (e: IOException) {
+                _searchUiState.update {
+                    it.copy(isLoading = false, error = e.message ?: "Unknown IO error")
+                }
+                Log.e("check2", "IOException", e)
+            } catch (e: Exception) {
+                _searchUiState.update {
+                    it.copy(isLoading = false, error = e.message ?: "Unknown error")
+                }
+                Log.e("check2", "Exception", e)
+            }
+
+        }
+    }
+
+    fun clearSearchData() {
+        _searchUiState.update {
+            it.copy(
+                items = null,
+                isLoading = false,
+                error = null)
+        }
+    }
+
+}
