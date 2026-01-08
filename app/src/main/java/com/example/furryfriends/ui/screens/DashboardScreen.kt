d03a4df042b4989eeef138e13b65d450f4ba8b64
Brogrammer7 <ct10025@tutanota.com>
2026-01-07 20:23:37 -0800

    Update TopAppBar and strings

--- app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
@@ -2,16 +2,19 @@ package com.example.furryfriends.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.furryfriends.R
 
 @Composable
@@ -29,23 +32,30 @@ fun FurryFriendsAppBar(
     titleText: String,
     modifier: Modifier = Modifier
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
             Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text = titleText,
                     maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
+                    overflow = TextOverflow.Ellipsis,
                 )
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        modifier = modifier
+        )
     )
 
+}
+
+
+@Preview
+@Composable
+fun DashboardScreenPreview() {
+    DashboardScreen(
+        titleText = stringResource(R.string.dashboard_screen_title))
 }
