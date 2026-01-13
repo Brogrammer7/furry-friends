e120684c11e0b9717a561799730666d6d8239c72
Brogrammer7 <ct10025@tutanota.com>
2026-01-13 01:42:21 -0800

    Update DashboardScreen

--- app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
@@ -1,9 +1,14 @@
 package com.example.furryfriends.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -12,15 +17,47 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.furryfriends.R
 
 @Composable
 fun DashboardScreen(
     modifier: Modifier = Modifier
 ) {
-    FurryFriendsAppBar(
-        titleText = stringResource(R.string.dashboard_screen_title)
-    )
+    Scaffold(
+        topBar = {
+            FurryFriendsAppBar(stringResource(R.string.dashboard_screen_title))
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Main content")
+            }
+
+            Text(
+                text = stringResource(R.string.dashboard_disclaimer),
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(horizontal = 16.dp),
+                color = MaterialTheme.colorScheme.onBackground,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
 }
 
 @Composable
@@ -43,11 +80,10 @@ fun FurryFriendsAppBar(
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
-        )
+        ),
+        modifier = modifier
     )
-
 }
-
 
 @Preview
 @Composable
