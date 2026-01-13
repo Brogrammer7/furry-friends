7897149dc9634dbbb9db6d72573a260e665f5d2b
Brogrammer7 <ct10025@tutanota.com>
2026-01-12 21:07:57 -0800

    Update strings and DashboardScreen

--- app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/DashboardScreen.kt
@@ -2,8 +2,6 @@ package com.example.furryfriends.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,12 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.furryfriends.R
 
 @Composable
 fun DashboardScreen(
-    titleText: String,
     modifier: Modifier = Modifier
 ) {
     FurryFriendsAppBar(
@@ -56,6 +52,5 @@ fun FurryFriendsAppBar(
 @Preview
 @Composable
 fun DashboardScreenPreview() {
-    DashboardScreen(
-        titleText = stringResource(R.string.dashboard_screen_title))
+    DashboardScreen()
 }
