14782931ea2d0e4a31365f77d456c616fb16e043
Brogrammer7 <ct10025@tutanota.com>
2026-01-12 22:43:25 -0800

    Add NavigationBar and NavGraph

--- app/src/main/java/com/example/furryfriends/MainActivity.kt
+++ app/src/main/java/com/example/furryfriends/MainActivity.kt
@@ -6,11 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Dashboard
+import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Dashboard
+import androidx.compose.material.icons.outlined.Pets
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.furryfriends.ui.screens.FindPetsScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.furryfriends.navigation.TabBarItem
+import com.example.furryfriends.navigation.TabView
+import com.example.furryfriends.ui.screens.DashboardScreen
 import com.example.furryfriends.ui.screens.SearchPetsScreen
 import com.example.furryfriends.ui.screens.SettingsScreen
 import com.example.furryfriends.ui.theme.FurryFriendsTheme
@@ -20,12 +33,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val dashboardTab = TabBarItem(
+                title = stringResource(R.string.dashboard_tab_title),
+                selectedIcon = Icons.Filled.Dashboard,
+                unselectedIcon = Icons.Outlined.Dashboard
+            )
+            val searchPetsTab = TabBarItem(
+                title = stringResource(R.string.search_pets_tab_title),
+                selectedIcon = Icons.Filled.Pets,
+                unselectedIcon = Icons.Outlined.Pets
+            )
+            val settingsTab = TabBarItem(
+                title = stringResource(R.string.settings_tab_title),
+                selectedIcon = Icons.Filled.Settings,
+                unselectedIcon = Icons.Outlined.Settings
+            )
+
+            val tabBarItems = listOf(dashboardTab, searchPetsTab, settingsTab)
+
+            val navController = rememberNavController()
 
             FurryFriendsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SearchPetsScreen(
+                Scaffold(
+                    bottomBar = { TabView(tabBarItems = tabBarItems, navController = navController) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = dashboardTab.title,
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable(dashboardTab.title) {
+                            DashboardScreen()
+                        }
+                        composable(searchPetsTab.title) {
+                            SearchPetsScreen()
+                        }
+                        composable(settingsTab.title) {
+                            SettingsScreen()
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/furryfriends/navigation/TabView.kt
@@ -0,0 +1,76 @@
+package com.example.furryfriends.navigation
+
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val iconTicker: Int? = null
+)
+
+@Composable
+fun TabView(tabBarItems: List<TabBarItem>, navController: NavController) {
+    var selectedTabIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        // looping over each tab to generate the views and navigation for each item
+        tabBarItems.forEachIndexed { index, tabBarItem ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    navController.navigate(tabBarItem.title)
+                },
+                icon = {
+                    TabBarIconView(
+                        isSelected = selectedTabIndex == index,
+                        selectedIcon = tabBarItem.selectedIcon,
+                        unselectedIcon = tabBarItem.unselectedIcon,
+                        title = tabBarItem.title,
+                        iconTicker = tabBarItem.iconTicker
+                    )
+                },
+                label = {Text(tabBarItem.title)})
+        }
+    }
+}
+
+@Composable
+fun TabBarIconView(
+    isSelected: Boolean,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    title: String,
+    iconTicker: Int? = null
+) {
+    BadgedBox(badge = { TabBarBadgeView(iconTicker) }) {
+        Icon(
+            imageVector = if (isSelected) {selectedIcon} else {unselectedIcon},
+            contentDescription = title
+        )
+    }
+}
+
+@Composable
+fun TabBarBadgeView(count: Int? = null) {
+    if (count != null) {
+        Badge {
+            Text(count.toString())
+        }
+    }
+}
