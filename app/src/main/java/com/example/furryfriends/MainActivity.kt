47e1260b8540f3892e1c28f973cfc033eb9d1886
Brogrammer7 <ct10025@tutanota.com>
2026-01-15 21:16:03 -0800

    Update imports and remove unnecessary layout

--- app/src/main/java/com/example/furryfriends/MainActivity.kt
+++ app/src/main/java/com/example/furryfriends/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Dashboard
@@ -16,20 +16,21 @@ import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Pets
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.furryfriends.navigation.TabBarItem
 import com.example.furryfriends.navigation.TabView
+import com.example.furryfriends.ui.screens.AboutScreen
 import com.example.furryfriends.ui.screens.DashboardScreen
 import com.example.furryfriends.ui.screens.SearchPetsScreen
 import com.example.furryfriends.ui.screens.SettingsScreen
 import com.example.furryfriends.ui.theme.FurryFriendsTheme
-import com.example.furryfriends.ui.screens.AboutScreen
+import com.example.furryfriends.ui.viewmodels.SearchPetsViewModel
+import com.example.furryfriends.ui.widgets.FurryFriendsAppBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,11 +63,10 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             fun getCurrentScreenTitle(): Int {
-                // Logic to return the current screen title based on navigation
                 return when (navController.currentDestination?.route) {
                     dashboardTab.title -> R.string.dashboard_screen_title
                     searchPetsTab.title -> R.string.search_pets_screen_title
-                    settingsTab.title -> R.string.settings_tab_title
+                    settingsTab.title -> R.string.settings_screen_title
                     aboutTab.title -> R.string.about_screen_title
                     else -> R.string.dashboard_screen_title
                 }
@@ -77,41 +77,25 @@ class MainActivity : ComponentActivity() {
                     topBar = { FurryFriendsAppBar(titleText = stringResource(getCurrentScreenTitle())) },
                     bottomBar = { TabView(tabBarItems = tabBarItems, navController = navController) }
                 ) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(), // Ensure padding is applied
-                        verticalArrangement = Arrangement.Top, // Change as needed
-                        horizontalAlignment = Alignment.CenterHorizontally // Change as needed
+                    NavHost(
+                        navController = navController,
+                        startDestination = dashboardTab.title,
                     ) {
-                        NavHost(
-                            navController = navController,
-                            startDestination = dashboardTab.title,
-                        ) {
-                            composable(dashboardTab.title) {
-                                DashboardScreen(Modifier.padding(innerPadding))
-                            }
-                            composable(searchPetsTab.title) {
-                                SearchPetsScreen(Modifier.padding(innerPadding))
-                            }
-                            composable(settingsTab.title) {
-                                SettingsScreen(Modifier.padding(innerPadding))
-                            }
-                            composable(aboutTab.title) {
-                                AboutScreen(Modifier.padding(innerPadding))
-                            }
+                        composable(dashboardTab.title) {
+                            DashboardScreen(Modifier.padding(innerPadding))
+                        }
+                        composable(searchPetsTab.title) {
+                            SearchPetsScreen(Modifier.padding(innerPadding))
+                        }
+                        composable(settingsTab.title) {
+                            SettingsScreen(Modifier.padding(innerPadding))
+                        }
+                        composable(aboutTab.title) {
+                            AboutScreen(Modifier.padding(innerPadding))
                         }
                     }
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FurryFriendsTheme {
-
     }
 }
