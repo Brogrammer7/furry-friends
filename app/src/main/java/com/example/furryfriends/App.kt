0764a36cd33aca907a02eefa1f6954131568737c
Brogrammer7 <ct10025@tutanota.com>
2026-06-24 03:31:58 -0700

    Cleanup from previous DI refactoring

--- app/src/main/java/com/example/furryfriends/App.kt
+++ app/src/main/java/com/example/furryfriends/App.kt
@@ -1,17 +1,7 @@
 package com.example.furryfriends
 
 import android.app.Application
-import com.example.furryfriends.data.PetsRepository
-import com.example.furryfriends.data.SettingsRepository
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-    val settingsRepository: SettingsRepository by lazy {
-        SettingsRepository(applicationContext)
-    }
-
-    val petsRepository: PetsRepository by lazy {
-        PetsRepository(applicationContext)
-    }
-}
+class App : Application()
