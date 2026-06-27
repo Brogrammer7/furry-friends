484f7bb9b1a149af1c7054340747e98a3cd6e4f3
Brogrammer7 <ct10025@tutanota.com>
2026-06-27 00:31:41 -0700

    Import cleanup

--- app/src/main/java/com/example/furryfriends/ui/screens/FindPetsScreen.kt
+++ app/src/main/java/com/example/furryfriends/ui/screens/FindPetsScreen.kt
@@ -1,17 +1,10 @@
 package com.example.furryfriends.ui.screens
 
-import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
@@ -20,18 +13,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil3.compose.AsyncImage
 import com.example.furryfriends.R
-import com.example.furryfriends.ui.viewmodels.FindPetsViewModel
 import com.example.furryfriends.ui.components.FurryFriendsAppBar
+import com.example.furryfriends.ui.viewmodels.FindPetsViewModel
 
 @Composable
 fun FindPetsScreen(
