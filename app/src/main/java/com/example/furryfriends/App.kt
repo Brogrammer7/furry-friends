1c3ed259deaa2b5a21b3c90eaafe119d184be417
Brogrammer7 <ct10025@tutanota.com>
2026-07-31 16:57:48 -0700

    Add notes for App class

--- app/src/main/java/com/example/furryfriends/App.kt
+++ app/src/main/java/com/example/furryfriends/App.kt
@@ -5,3 +5,14 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application()
+
+/*
+* Dependency Injection is initialized here (already done above with annotation)
+*
+* Initialize future analytics here:
+* ex: Analytics.init(this)
+*
+* Set up future logging here:
+* ex: Logger.init()
+*
+* */
